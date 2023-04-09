a4fd37eb1975607bdb4c7766dffe04ae71ef24fc
Roman K <39156575+RV87K@users.noreply.github.com>
2023-04-09 20:01:47 +1000

    feat: add posts object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Counter from './components/Counter';
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
@@ -6,6 +6,7 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from './components/UI/button/MyButton';
 import MyInput from "./components/UI/input/MyInput";
+
 
 function App() {
 
@@ -15,15 +16,35 @@ function App() {
     { id: 3, title: 'Javascript 3', body: 'Description' }
   ])
 
+  const [post, setPost] = useState({title:'', body:''});
+  
+
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+
+    setPosts([...posts, {...post, id:Date.now()}])
+    setPost({title:'', body:''})
+  }
 
   return (
     <div className="App">
       <form>
-        <MyInput type="text" placeholder="Название поста"/>
-        <MyInput type="text" placeholder="Описание поста"/>
-        <MyButton>Создать пост</MyButton>
+        <MyInput
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
-      <PostList posts={posts} title={'Посты про JS'}/>
+      <PostList posts={posts} title={'Посты про JS'} />
     </div >
   );
 }
